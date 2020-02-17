cf6212924166fa65ee94b243d09b1fb05b8daf3f
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-17 02:58:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,10 +22,10 @@ class CashRegister
 
   def apply_discount
     if @discount == 0
-      return "There is no discount to apply."
+    "There is no discount to apply."
     end
     @total -= @total * @discount / 100
-    return "After the discount, the total comes to $#{@total}."
+    "After the discount, the total comes to $#{@total}."
   end 
 
   def items
